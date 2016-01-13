bc28b483a775761f417b9f82320018ac6afc2e59
ares <ares@git.git>
2016-01-13 10:31:50 +0100

    6

--- JEE2/src/springapp/model/CV.java
+++ JEE2/src/springapp/model/CV.java
@@ -1,23 +1,52 @@
 package springapp.model;
 
 import java.io.Serializable;
+import java.util.Set;
 
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity(name="CV")
 public class CV implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-@Id()
-@Basic(optional = false)
-@Column(name = "numCV", length = 200,
-        nullable = false)
-String numCV;
+	@Id()
+	@Basic(optional = false)
+	@Column(name = "numCV", length = 200,
+	        nullable = false)
+	String numCV;
 
+	@OneToMany
+    private Set<Activities> activities;
 
+	public CV() {
+		super();
+	}
+
+	public CV(String numCV, Set<Activities> activities) {
+		super();
+		this.numCV = numCV;
+		this.activities = activities;
+	}
+
+	public String getNumCV() {
+		return numCV;
+	}
+
+	public void setNumCV(String numCV) {
+		this.numCV = numCV;
+	}
+
+	public Set<Activities> getActivities() {
+		return activities;
+	}
+
+	public void setActivities(Set<Activities> activities) {
+		this.activities = activities;
+	}
 
 }
