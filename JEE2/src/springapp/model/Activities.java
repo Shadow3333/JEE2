172f7465538cb65f449d371f58a7a83a0e2b6de4
ares <ares@git.git>
2016-01-13 10:27:02 +0100

    5

--- JEE2/src/springapp/model/Activities.java
+++ JEE2/src/springapp/model/Activities.java
@@ -12,12 +12,80 @@ public class Activities implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-@Id()
-@Basic(optional = false)
-@Column(name = "idActiv", length = 200,
-        nullable = false)
-String idActiv;
-
-
+	@Id()
+	@Basic(optional = false)
+	@Column(name = "idActiv", length = 200,
+	        nullable = false)
+	private String idActiv;
 	
+	@Basic(optional = false)
+	private String year;
+	
+	@Basic(optional = false)
+	private String nature;
+	
+	@Basic(optional = false)
+	private String titre;
+	
+	private String description;
+	
+	private String web;
+	
+	public Activities() {
+		super();
+	}
+	public Activities(String idActiv, String year, String nature, String titre) {
+		super();
+		this.idActiv = idActiv;
+		this.year = year;
+		this.nature = nature;
+		this.titre = titre;
+	}
+	public Activities(String idActiv, String year, String nature, String titre, String description, String web) {
+		super();
+		this.idActiv = idActiv;
+		this.year = year;
+		this.nature = nature;
+		this.titre = titre;
+		this.description = description;
+		this.web = web;
+	}
+	public String getIdActiv() {
+		return idActiv;
+	}
+	public void setIdActiv(String idActiv) {
+		this.idActiv = idActiv;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getNature() {
+		return nature;
+	}
+	public void setNature(String nature) {
+		this.nature = nature;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getWeb() {
+		return web;
+	}
+	public void setWeb(String web) {
+		this.web = web;
+	}
+	
+
 }
